1224a14c49462efb9d338c5a0a680bb9d4ee4cf0
Sharat Hegde <sharath@testvagrant.com>
2023-07-26 16:09:10 +0530

    After WebDriver Import

--- src/main/java/org/example/pages/HomePage.java
+++ src/main/java/org/example/pages/HomePage.java
@@ -1,15 +1,46 @@
 package org.example.pages;
 import org.example.models.Item;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class HomePage {
+    WebDriver webDriver;
+    By searchIcon = By.cssSelector("summary[aria-label='Search']");
+    By searchBar = By.id("Search-In-Modal");
+    By searchResults = By.cssSelector("li[id^='predictive-search-option'] a");
+
+    // Scoped Element
+    By productName = By.cssSelector(".predictive-search__item-heading");
+
     public HomePage(WebDriver webDriver) {
+        this.webDriver = webDriver;
     }
-    public void search(String searchItem) {
+
+    public HomePage search(String searchItem) {
+        webDriver.findElement(searchIcon).click();
+        webDriver.findElement(searchBar).sendKeys(searchItem);
+        return this;
     }
+
     public List<Item> getSearchItems() {
-        return new ArrayList<>();
+        List<WebElement> elements = webDriver.findElements(searchResults);
+        List<Item> items = new ArrayList<>();
+        for(WebElement element : elements) {
+            String name = element.findElement(productName).getText();
+            Item item = new Item();
+            item.setName(name);
+            items.add(item);
+        }
+        return items;
+    }
+
+    public int getItemCount(){
+        List<Item> items = new ArrayList<>();
+       items = getSearchItems();
+       return items.size();
     }
 }
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,4 +1,5 @@
 
+import org.example.drivers.DriverCreator;
 import org.example.models.Item;
 import org.example.pages.HomePage;
 import org.example.pages.LauncherPage;
@@ -9,12 +10,14 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public  class SearchTest{
+
+
     @Test
     public void verifyIfSearchTermShowsRelevantResults() {
-        //Arrange
-        String searchItem = "Product";
-        String searchKey = "Product";
-        WebDriver webDriver = null; //Dummy WebDriver declaration
+        String searchItem = "Jeans";
+        String searchKey = "Jean";
+        String browser = "chrome";
+        WebDriver webDriver = new DriverCreator().create(browser);
         LauncherPage launcherPage = new LauncherPage(webDriver);
         launcherPage.navigateTo("https://web-playground.ultralesson.com/");
 
@@ -27,4 +30,60 @@ public  class SearchTest{
         Assert.assertEquals(4, searchItems.size());
         Assert.assertTrue(searchItems.stream().allMatch(item -> item.getName().contains(searchKey)));
     }
+    @Test
+    public void verifySearchUnavailableProduct() {
+        // Arrange
+        String unavailableProduct = "Unobtainium Widget";
+        WebDriver webDriver = null;
+
+        LauncherPage launcherPage = new LauncherPage(webDriver);
+        launcherPage.navigateTo("https://web-playground.ultralesson.com/");
+
+        // Act
+        HomePage homepage = new HomePage(webDriver);
+        homepage.search(unavailableProduct);
+        List<Item> searchItems = homepage.getSearchItems();
+
+        // Assert
+        Assert.assertTrue(searchItems.isEmpty());
+    }
+
+    @Test
+    public void verifyBrandSearchListsOnlyBrandItems() {
+        // Arrange
+        String brandName = "Nike";
+        WebDriver webDriver = null;
+
+        LauncherPage launcherPage = new LauncherPage(webDriver);
+        launcherPage.navigateTo("https://web-playground.ultralesson.com/");
+
+        // Act
+        HomePage homepage = new HomePage(webDriver);
+        homepage.search(brandName);
+        List<Item> searchItems = homepage.getSearchItems();
+
+        // Assert
+        Assert.assertTrue(searchItems.stream().allMatch(item -> item.getName().contains(brandName)));
+    }
+
+
+    @Test
+    public void verifySearchResultCountMatchesDisplayedItems() {
+        // Arrange
+        String searchItem = "Shoes";
+        WebDriver webDriver = null;
+
+        LauncherPage launcherPage = new LauncherPage(webDriver);
+        launcherPage.navigateTo("https://web-playground.ultralesson.com/");
+
+        // Act
+        HomePage homepage = new HomePage(webDriver);
+        homepage.search(searchItem);
+        List<Item> searchItems = homepage.getSearchItems();
+        int itemCountDisplayed = homepage.getItemCount(); // Assume getItemCount method returns the number displayed on the page
+
+        // Assert
+        Assert.assertEquals(searchItems.size(), itemCountDisplayed);
+    }
+
 }
